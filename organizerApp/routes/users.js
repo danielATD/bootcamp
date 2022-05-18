b5fdfa4f441b9f4c53029cce8a7cd1d6dc554d17
Daniel <danieltd@Daniels-MacBook-Air.local>
2022-05-17 20:15:37 -0500

    Configuracion de la BD y queries

--- organizerApp/routes/users.js
+++ organizerApp/routes/users.js
@@ -3,6 +3,16 @@ const database = require("../libs/database")
 
 const router = express.Router()
 
+router.get("/users",function(req,res){     
+    database.connection.query("SELECT * FROM users", function(error,result){   //Haciendo una consulta a la base de datos desde la ruta users
+        console.log(error)
+        console.log(result)
+    })
+    res.json({
+        ruta: "users"
+    })
+})
+
 router.get("/users",function(req,res){
     res.json({
         ruta:"users"
